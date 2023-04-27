19b1d005fe288671954f846dcee0d20f52e88004
royeeliu <royeeliu@qq.com>
2023-04-27 21:17:08 +0800

    show image with softbuffer

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use image::GenericImageView;
+use std::num::NonZeroU32;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
+fn main() {
+    let lena = image::load_from_memory(include_bytes!("../res/lena.jpg")).unwrap();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_inner_size(winit::dpi::PhysicalSize::new(lena.width(), lena.height()))
+        .build(&event_loop)
+        .unwrap();
+
+    let context = unsafe { softbuffer::Context::new(&window) }.unwrap();
+    let mut surface = unsafe { softbuffer::Surface::new(&context, &window) }.unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::RedrawRequested(window_id) if window_id == window.id() => {
+                surface
+                    .resize(
+                        NonZeroU32::new(lena.width()).unwrap(),
+                        NonZeroU32::new(lena.height()).unwrap(),
+                    )
+                    .unwrap();
+
+                let mut buffer = surface.buffer_mut().unwrap();
+                let width = lena.width() as usize;
+                for (x, y, pixel) in lena.pixels() {
+                    let red = pixel.0[0] as u32;
+                    let green = pixel.0[1] as u32;
+                    let blue = pixel.0[2] as u32;
+
+                    let color = blue | (green << 8) | (red << 16);
+                    buffer[y as usize * width + x as usize] = color;
+                }
+
+                buffer.present().unwrap();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => {
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => {}
+        }
+    });
+}
