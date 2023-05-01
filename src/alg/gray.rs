df174e046a78214492da30342520ab14b28f27a5
royeeliu <royeeliu@qq.com>
2023-05-01 22:38:45 +0800

    histogram equalization

--- src/alg/gray.rs
+++ src/alg/gray.rs
@@ -14,3 +14,24 @@ pub fn average_gray_level(image: &GrayImage) -> u8 {
     let sum: u64 = pixels.into_iter().map(|x| x.0[0] as u64).sum();
     (sum / len) as u8
 }
+
+pub fn histogram_equalize(image: &GrayImage) -> GrayImage {
+    let mut gray_count = [0u64; 256];
+    for luma in image.pixels() {
+        gray_count[luma.0[0] as usize] += 1;
+    }
+
+    let mut gray_map = [0u8; 256];
+    let len = image.width() as u64 * image.height() as u64;
+    let mut sum = 0u64;
+    for i in 0..256 {
+        sum += gray_count[i];
+        gray_map[i] = ((sum * 255 + len / 2) / len) as u8;
+    }
+
+    let mut out = image.clone();
+    for luma in out.iter_mut() {
+        *luma = gray_map[*luma as usize];
+    }
+    out
+}
--- src/proc.rs
+++ src/proc.rs
@@ -1,17 +1,20 @@
-use image::{DynamicImage, GenericImageView, Rgba, RgbaImage};
-use imageproc::drawing::draw_line_segment_mut;
+use image::{DynamicImage, GenericImageView, GrayImage, Pixel, Rgba, RgbaImage};
+use imageproc::{
+    drawing::{draw_filled_rect_mut, draw_line_segment_mut},
+    rect::Rect,
+};
 
 use crate::{
-    alg::{self, gray::average_gray_level},
+    alg::{self, gray::average_gray_level, gray::histogram_equalize},
     draw::ImageDrawer,
 };
 
-pub fn gray(image: image::DynamicImage) -> Vec<ImageDrawer> {
+pub fn gray(image: DynamicImage) -> Vec<ImageDrawer> {
     let gray_image = image.to_luma8();
     vec![ImageDrawer::from(image), ImageDrawer::from(gray_image)]
 }
 
-pub fn binary(image: image::DynamicImage, threshold: Option<u8>) -> Vec<ImageDrawer> {
+pub fn binary(image: DynamicImage, threshold: Option<u8>) -> Vec<ImageDrawer> {
     let gray_image = image.to_luma8();
     let level = match threshold {
         Some(v) => v,
@@ -26,10 +29,53 @@ pub fn binary(image: image::DynamicImage, threshold: Option<u8>) -> Vec<ImageDra
     ]
 }
 
-pub fn histogram(image: image::DynamicImage) -> Vec<ImageDrawer> {
-    let gray_image = image.to_luma8();
+fn create_histogram_canvas() -> RgbaImage {
     let background = Rgba([255u8, 255u8, 255u8, 255u8]);
     let gray = Rgba([128u8, 128u8, 128u8, 255u8]);
+    let mut image = RgbaImage::from_pixel(512, 512, background);
+    draw_line_segment_mut(&mut image, (0.0, 127.0), (511.0, 127.0), gray);
+    draw_line_segment_mut(&mut image, (0.0, 255.0), (511.0, 255.0), gray);
+    draw_line_segment_mut(&mut image, (0.0, 383.0), (511.0, 383.0), gray);
+    draw_line_segment_mut(&mut image, (0.0, 511.0), (511.0, 511.0), gray);
+    image
+}
+
+fn draw_histogram_part(
+    canvas: &mut RgbaImage,
+    values: &[u64; 256],
+    scale: u64,
+    vertical_range: (i32, i32),
+    color: Rgba<u8>,
+) {
+    let height = (vertical_range.1 - vertical_range.0) as u64;
+    for (i, v) in values.iter().enumerate() {
+        let x = (i * 2) as i32;
+        let h = ((v * height + (scale / 2)) / scale) as i32;
+        if h > 0 {
+            let rect = Rect::at(x, vertical_range.1 - h).of_size(2, h as u32);
+            draw_filled_rect_mut(canvas, rect, color);
+        }
+    }
+}
+
+fn draw_histogram_scale_gray(image: &GrayImage, scale: Option<u64>) -> (DynamicImage, u64) {
+    let black = Rgba([0u8, 0u8, 0u8, 255u8]);
+    let mut gray_count = [0u64; 256];
+    for luma in image.pixels() {
+        gray_count[luma.0[0] as usize] += 1;
+    }
+    let scale = if let Some(v) = scale {
+        v
+    } else {
+        gray_count.iter().map(|v| *v).max().unwrap()
+    };
+
+    let mut canvas = create_histogram_canvas();
+    draw_histogram_part(&mut canvas, &gray_count, scale, (384, 511), black);
+    (DynamicImage::from(canvas), scale)
+}
+
+fn draw_histogram_scale(image: &DynamicImage, scale: Option<u64>) -> (DynamicImage, u64) {
     let black = Rgba([0u8, 0u8, 0u8, 255u8]);
     let red = Rgba([255u8, 0u8, 0u8, 255u8]);
     let green = Rgba([0u8, 255u8, 0u8, 255u8]);
@@ -39,64 +85,49 @@ pub fn histogram(image: image::DynamicImage) -> Vec<ImageDrawer> {
     let mut red_count = [0u64; 256];
     let mut green_count = [0u64; 256];
     let mut blue_count = [0u64; 256];
-    for pixel in gray_image.pixels() {
-        gray_count[pixel.0[0] as usize] += 1;
-    }
     for (_, _, pixel) in image.pixels() {
+        let luma = pixel.to_luma();
+        gray_count[luma.0[0] as usize] += 1;
         red_count[pixel.0[0] as usize] += 1;
         green_count[pixel.0[1] as usize] += 1;
         blue_count[pixel.0[2] as usize] += 1;
     }
-    let max = [
-        gray_count.iter().map(|v| *v).max().unwrap(),
-        red_count.iter().map(|v| *v).max().unwrap(),
-        green_count.iter().map(|v| *v).max().unwrap(),
-        blue_count.iter().map(|v| *v).max().unwrap(),
-    ]
-    .iter()
-    .map(|v| *v)
-    .max()
-    .unwrap();
-    let scale = 127.0 / max as f32;
+    let scale = if let Some(v) = scale {
+        v
+    } else {
+        let max = [
+            gray_count.iter().map(|v| *v).max().unwrap(),
+            red_count.iter().map(|v| *v).max().unwrap(),
+            green_count.iter().map(|v| *v).max().unwrap(),
+            blue_count.iter().map(|v| *v).max().unwrap(),
+        ]
+        .into_iter()
+        .max()
+        .unwrap();
+        max
+    };
 
-    let mut hist_image = RgbaImage::from_pixel(512, 512, background);
-    draw_line_segment_mut(&mut hist_image, (0.0, 128.0), (511.0, 128.0), gray);
-    draw_line_segment_mut(&mut hist_image, (0.0, 256.0), (511.0, 256.0), gray);
-    draw_line_segment_mut(&mut hist_image, (0.0, 382.0), (511.0, 382.0), gray);
+    let mut canvas = create_histogram_canvas();
+    draw_histogram_part(&mut canvas, &red_count, scale, (0, 127), red);
+    draw_histogram_part(&mut canvas, &green_count, scale, (128, 255), green);
+    draw_histogram_part(&mut canvas, &blue_count, scale, (256, 383), blue);
+    draw_histogram_part(&mut canvas, &gray_count, scale, (384, 511), black);
 
-    for (i, v) in red_count.iter().enumerate() {
-        let x = i as f32 * 2.0;
-        let h = *v as f32 * scale;
-        if h > 0.001 {
-            draw_line_segment_mut(&mut hist_image, (x, 127.0 - h), (x, 127.0), red);
-        }
-    }
-    for (i, v) in green_count.iter().enumerate() {
-        let x = i as f32 * 2.0;
-        let h = *v as f32 * scale;
-        if h > 0.001 {
-            draw_line_segment_mut(&mut hist_image, (x, 255.0 - h), (x, 255.0), green);
-        }
-    }
-    for (i, v) in blue_count.iter().enumerate() {
-        let x = i as f32 * 2.0;
-        let h = *v as f32 * scale;
-        if h > 0.001 {
-            draw_line_segment_mut(&mut hist_image, (x, 381.0 - h), (x, 381.0), blue);
-        }
-    }
-    for (i, v) in gray_count.iter().enumerate() {
-        let x = i as f32 * 2.0;
-        let h = *v as f32 * scale;
-        if h > 0.001 {
-            draw_line_segment_mut(&mut hist_image, (x, 511.0 - h), (x, 511.0), black);
-        }
-    }
+    (DynamicImage::from(canvas), scale)
+}
 
+pub fn histogram(image: DynamicImage) -> Vec<ImageDrawer> {
     let gray_image = image.to_luma8();
+    let equalized_image = histogram_equalize(&gray_image);
+
+    let (hist_original, scale) = draw_histogram_scale(&image, None);
+    let hist_equalized = draw_histogram_scale_gray(&equalized_image, Some(scale)).0;
+
     vec![
         ImageDrawer::from(image),
         ImageDrawer::from(gray_image),
-        ImageDrawer::from(DynamicImage::from(hist_image)),
+        ImageDrawer::from(equalized_image),
+        ImageDrawer::from(hist_original),
+        ImageDrawer::from(hist_equalized),
     ]
 }
